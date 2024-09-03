19142c742c4df903f30d08e429f7c1121a535d52
jsw <csw9201@naver.com>
2024-09-03 10:39:08 +0900

    React memozation 확인(api)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,22 @@
-import "./globals.css";
-import Link from "next/link";
-import style from "./layout.module.css";
+import './globals.css';
+import Link from 'next/link';
+import style from './layout.module.css';
+import { BookData } from '@/types';
+
+const Footer = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  if (!response.ok) return <footer>제작 @winterlood</footer>;
+  const allBooks: BookData[] = await response.json();
+  const bookCount = allBooks.length;
+  return (
+    <footer>
+      <div>제작 @winterlood</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다</div>
+    </footer>
+  );
+};
 
 export default function RootLayout({
   children,
@@ -8,14 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <div className={style.container}>
           <header>
-            <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+            <Link href={'/'}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
